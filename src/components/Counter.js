ae228687ac88f6a973609da23e118e717850b24d
Lukas <lukas.ancikevicius@gmail.com>
2021-09-08 14:18:07 +0300

    add up

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,38 @@
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 
 export default function Counter() {
-  return <div>Counter is here</div>;
+  const [number, setNumber] = useState(0);
+  const [themeOn, setThemeOn] = useState(false);
+  function slowFunction(num) {
+    for (let i = 0; i < 100000000; i++) {
+      const result = num * 2;
+
+      return num * 2;
+    }
+  }
+  function themeHandler() {
+    setThemeOn((prevState) => !prevState);
+  }
+  const numberCalculated = useMemo(() => {
+    return slowFunction(number);
+  }, [number]);
+
+  return (
+    <div
+      style={{
+        backgroundColor: themeOn ? 'black' : 'white',
+        color: themeOn ? 'white' : 'black',
+      }}
+    >
+      <h1>Counter</h1>
+      <input
+        value={number}
+        onChange={(e) => setNumber(e.target.value)}
+        type="number"
+        placeholder="enter number"
+      />
+      <button onClick={themeHandler}>Toggle theme</button>
+      <h2>{numberCalculated}</h2>
+    </div>
+  );
 }
